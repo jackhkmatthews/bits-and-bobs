25c5c1ba716580bfdb29613d7ff66ad61e2eea21
Jack Matthews <jackmatthews@tabxps.stink.tv>
2017-04-13 10:53:00 +0100

    created program and attached shaders

--- WebGL-indigo-code/app.js
+++ WebGL-indigo-code/app.js
@@ -1,3 +1,25 @@
+var vertexShaderText =
+[
+'precision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'',
+'void main()',
+'{',
+'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float;',
+'',
+'void main()',
+'{',
+'  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+'}'
+].join('\n');
+
 var InitDemo = function () {
 	console.log('loaded');
 
@@ -6,5 +28,37 @@ var InitDemo = function () {
 
 	gl.clearColor(0.75, 0.85, 0.8, 1);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+	gl.shaderSource(vertexShader, vertexShaderText);
+	gl.shaderSource(fragmentShader, fragmentShaderText);
+
+	gl.compileShader(vertexShader);
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+		return;
+	}
+
+	gl.compileShader(fragmentShader);
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+		console.error('ERROR compiling fragmentShader shader!', gl.getShaderInfoLog(fragmentShader));
+		return;
+	}
+
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+		console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+		return;
+	}
+	gl.validateProgram(program);
+	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+		console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+		return;
+	}
 
 }
