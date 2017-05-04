d1b320c265b81e08cf6a9b6181719c7be561cfd9
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-04 18:14:19 +0100

    created a service to handle the fetching of mock hero data. Included in the providers array of the component.

--- angular.io-tutorial/quickstart/src/app/hero.service.ts
+++ angular.io-tutorial/quickstart/src/app/hero.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heros';
 
-@Injectable()
-export class Heroservice {
-  
+// @Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
 }
