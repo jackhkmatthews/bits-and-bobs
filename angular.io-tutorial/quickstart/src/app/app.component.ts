01ef45f61d0e816dcd780877145447a079341e30
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-04 17:30:51 +0100

    se[erated out the hero detail code into it's own component and passed information from it's parent to it

--- angular.io-tutorial/quickstart/src/app/app.component.ts
+++ angular.io-tutorial/quickstart/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -23,20 +20,13 @@ const HEROES: Hero[] = [
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="let hero of heroes" (click)="onSelect(hero
-        )" [class.selected]="hero === selectedHero">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div>
-        <label>id: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-      <div><label>name: </label>{{selectedHero.name}}</div>
-    </div>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero
+      )" [class.selected]="hero === selectedHero">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
     .selected {
--- angular.io-tutorial/quickstart/src/app/app.module.ts
+++ angular.io-tutorial/quickstart/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // ngModel lives in here
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
