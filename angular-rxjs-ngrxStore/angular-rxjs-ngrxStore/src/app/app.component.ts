9118b6a5a9c1a3292a0e900f68670f81b318db0a
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-12 11:04:24 +0100

    RxJS startWith and scan

--- angular-rxjs-ngrxStore/angular-rxjs-ngrxStore/src/app/app.component.ts
+++ angular-rxjs-ngrxStore/angular-rxjs-ngrxStore/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/scan';
 import {Subject} from 'rxjs/Subject';
 
 @Component({
@@ -15,8 +18,14 @@ export class AppComponent {
   clock;
 
   constructor() {
-    this.clock = this.click$.map(() => {
-      return new Date();
-    });
+    this.clock = Observable.merge(
+      this.click$,
+      Observable.interval(1000)
+    ).startWith(new Date())
+      .scan((accumulator: Date, current) => {
+        console.log(accumulator.getTime());
+
+        return new Date((accumulator.getTime() + 1000));
+      });
   }
 }
