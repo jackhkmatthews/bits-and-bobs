7a6b167f4845f576b9c41edd272f702cba11d1a9
Jack Matthews <jackhkmatthews@gmail.com>
2017-07-14 15:35:49 +0100

    validation in sequelize models

--- sequelize-tutorial/app.js
+++ sequelize-tutorial/app.js
@@ -1,20 +1,64 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('sequelize_demo', 'jackmatthews', 'password', {
+const sequelize = new Sequelize('sequelize_demo', 'jackmatthews', 'password', {
   host: 'localhost',
   dialect: 'postgres'
 });
 
-const Article = connection.define('article', {
-  title: Sequelize.STRING,
-  body: Sequelize.TEXT
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Article = sequelize.define('article', {
+  slug: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [10, 100],
+        msg: 'Please enter a title between 10 and 100'
+      }
+    }
+  },
+  body: {
+    type: Sequelize.TEXT,
+    defaultValue: 'Coming soon...',
+    validate: {
+      startsWithUpper: bodyval => {
+        const first = bodyval.charAt(0);
+        var startsWithUpper = first === first.toUpperCase();
+        if (!startsWithUpper) {
+          throw new Error('First letter must be a uppercase letter');
+        } else {
+          // ..
+        }
+      }
+    }
+  }
+}, {
+  timestamps: false
 });
 
-connection.sync()
+Article
+  .sync({force: true})
   .then(() => {
-    Article.findAll()
-      .then(articles => {
-        console.log(articles.length);
-      });
+    return Article.create({
+      title: 'Hello there',
+      slug: 'hello-there',
+      body: 'A nothing article about saying hello in different languages'
+    });
+  })
+  .catch(error => {
+    console.log(error);
   });
 
