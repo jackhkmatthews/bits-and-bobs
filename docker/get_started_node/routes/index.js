c6a238f54b1284bbcc98a0fdf30512165f8d0f33
Jack Matthews <jackhkmatthews@gmail.com>
2017-09-08 14:14:30 +0100

    services, swarms and stacks. Serverside counter inplemented

--- docker/get_started_node/routes/index.js
+++ docker/get_started_node/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { Client } = require('pg');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, expRes, next) {
+  const client = new Client({
+    user: 'postgres',
+    host: '127.0.0.1',
+    password: 'postgres',
+    database: 'getstartednode'
+  });
+  client.connect();
+  client.query(`INSERT INTO counter VALUES (1); SELECT * FROM counter`, (err, res) => {
+    client.end();
+    if (err) {
+      console.log('error', err);
+    }
+    if (res) console.log(res[1].rowCount);
+    expRes.render('index', { title: 'Express', counter: res[1].rowCount });
+  });
 });
 
 module.exports = router;
